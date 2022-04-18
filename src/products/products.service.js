77c0bb01806adb2a1d8377c79845d702aaf402e2
dan <da.ga.mo.94@gmail.com>
2022-04-17 23:39:31 -0600

    completed lesson 35.8

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -2,15 +2,45 @@ const knex = require("../db/connection");
 
 const productsTable = "products";
 
-function list(){
+function list() {
     return knex(productsTable).select("*");
 }
 
-function read(productId){
-    return knex(productsTable).select("*").where({product_id: productId}).first();
+function read(productId) {
+    return knex(productsTable).select("*").where({ product_id: productId }).first();
+}
+
+function listOutOfStockCount() {
+    return knex(productsTable)
+        .select("product_quantity_in_stock as out_of_stock")
+        .count("product_id")
+        .where({ product_quantity_in_stock: 0 })
+        .groupBy("out_of_stock");
+}
+
+function listPriceSummary() {
+    return knex(productsTable)
+        .select("supplier_id")
+        .min("product_price")
+        .max("product_price")
+        .avg("product_price")
+        .groupBy("supplier_id");
+}
+
+function listTotalWeightByProduct() {
+    return knex(productsTable)
+        .select(
+            "product_sku",
+            "product_title",
+            knex.raw("sum(product_weight_in_lbs * product_quantity_in_stock) as total_weight_in_lbs")
+        )
+        .groupBy("product_title", "product_sku");
 }
 
 module.exports = {
     list,
-    read
+    read,
+    listOutOfStockCount,
+    listPriceSummary,
+    listTotalWeightByProduct
 }
